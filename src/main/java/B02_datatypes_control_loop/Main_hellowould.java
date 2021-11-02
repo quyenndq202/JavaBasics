fa200bd47db7fee015733c29615afc42d4f784f9
quyenndq202 <93115630+quyenndq202@users.noreply.github.com>
2021-11-02 11:53:52 +0700

    Initial commit

---
+++ src/main/java/B02_datatypes_control_loop/Main_hellowould.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package B02_datatypes_control_loop;
+
+/**
+ *
+ * @author DUC QUYEN Vlog
+ */
+public class Main_hellowould {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World")
+}
